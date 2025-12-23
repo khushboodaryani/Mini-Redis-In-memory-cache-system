c1a23b203f26a1621522039668b80d380221684f
khushboodaryani <khushboodaryani1@gmail.com>
2025-12-23 19:52:51 +0530

    TTLCache.h updated

---
+++ TTLCache.h
@@ -0,0 +1,31 @@
+#ifndef TTLCACHE_H
+#define TTLCACHE_H
+#include <unordered_map>
+#include "Cache.h"
+#include<chrono>
+#include<list>
+
+using namespace std;
+
+class TTLCache :Cache{
+   private:
+    int ttlSeconds;
+
+    // list<pair<int, chrono::steady_clock>>dll;
+
+    // key -> {value, expiry_time}
+    //expire logic-- if ( current_time > expiry_time)--> delete
+   unordered_map<int, list<pair<int, chrono:: steady_clock>>::iterator> cache; // safe clock (never goes backward)
+    // time_point -> exact momemnt
+    // lazy expire
+    // if key exist check its current time vs exoire time and delete it else return value
+    public:
+     TTLCache(int ttl);
+     int get(int key) override;
+     void put(int key , int value)override;
+
+
+};
+
+
+#endif
